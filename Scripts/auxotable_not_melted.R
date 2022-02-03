ea1a7b7faa13d92ed13cdf22906d1d4b719e3d64
Svenja Busche <s.busche@nutrinf.uni-kiel.de>
2022-02-03 10:39:52 +0000

    Update auxotable_not_melted.R

--- Scripts/auxotable_not_melted.R
+++ Scripts/auxotable_not_melted.R
@@ -11,3 +11,27 @@ Auxotrophy_12 <- merge(Auxotrophy, Metadata, by.x = "Genomes",
                        by.y = "HRGM name")
 #change column name
 names(Auxotrophy_12) [names(Auxotrophy_12) == "Completeness (%)"] <- "Completeness"
+
+Auxotrophy_12$Status <- ifelse(Auxotrophy_12$count == 0, 1, 0)
+Auxotrophy_12[, phylum := str_match(`GTDB Taxonomy`, "p__.*;c__")[,1]]
+Auxotrophy_12[, phylum := gsub("p__|;c__","", phylum)]
+Auxotrophy_12[, phylum := gsub("_C$","", phylum)]
+
+Auxotrophy_12$phylum[Auxotrophy_12$phylum == "Firmicutes_A"] <- "Firmicutes"
+Auxotrophy_12$phylum[Auxotrophy_12$phylum == "Firmicutes_B"] <- "Firmicutes"
+Auxotrophy_12$phylum[Auxotrophy_12$phylum== "Firmicutes_G"] <- "Firmicutes"
+Auxotrophy_12$phylum[Auxotrophy_12$phylum == "Firmicutes_I"] <- "Firmicutes"
+Auxotrophy_12$phylum[Auxotrophy_12$phylum == "Bdellovibrionota"] <- "Other"
+Auxotrophy_12$phylum[Auxotrophy_12$phylum == "Campylobacterota"] <- "Other"
+Auxotrophy_12$phylum[Auxotrophy_12$phylum == "Cyanobacteria"] <- "Other"
+Auxotrophy_12$phylum[Auxotrophy_12$phylum == "Desulfobacterota_A"] <- "Other"
+Auxotrophy_12$phylum[Auxotrophy_12$phylum == "Elusimicrobiota"] <- "Other"
+Auxotrophy_12$phylum[Auxotrophy_12$phylum == "Eremiobacterota"] <- "Other"
+Auxotrophy_12$phylum[Auxotrophy_12$phylum == "Fibrobacterota"] <- "Other"
+Auxotrophy_12$phylum[Auxotrophy_12$phylum == "Halobacterota"] <- "Other"
+Auxotrophy_12$phylum[Auxotrophy_12$phylum == "Myxococcota"] <- "Other"
+Auxotrophy_12$phylum[Auxotrophy_12$phylum == "Patescibacteria"] <- "Other"
+Auxotrophy_12$phylum[Auxotrophy_12$phylum == "Synergistota"] <- "Other"
+Auxotrophy_12$phylum[Auxotrophy_12$phylum == "Spirochaeotota"] <- "Other"
+Auxotrophy_12$phylum[Auxotrophy_12$phylum == "Thermoplasmatota"] <- "Other"
+Auxotrophy_12$phylum[Auxotrophy_12$phylum == "Verrucomicrobiota"] <- "Other"
