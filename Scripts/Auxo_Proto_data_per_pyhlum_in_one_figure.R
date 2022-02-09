aed3313cd290dc1fd1bbb03080caa8c3ab029a8b
Svenja Busche <s.busche@nutrinf.uni-kiel.de>
2022-02-09 10:07:28 +0100

    created a loop to shorten the scriptcode

--- Scripts/Auxo_Proto_data_per_pyhlum_in_one_figure.R
+++ Scripts/Auxo_Proto_data_per_pyhlum_in_one_figure.R
@@ -2,30 +2,27 @@
 
 Auxotrophy_12 <- Auxotrophy_12[complete.cases(Auxotrophy_12), ]
 
+relphylum <- unique(Auxotrophy_12$phylum)
+prop_all <- list()
+k <- 1
+for (pi in relphylum) {
+  Auxo_all <- nrow(Auxotrophy_12[Auxotrophy_12$phylum == pi & Auxotrophy_12$Status == 0])
+  Proto_all <- nrow(Auxotrophy_12[Auxotrophy_12$phylum == pi & Auxotrophy_12$Status == 1])
+  p <- rbind(Auxo_all,Proto_all)
+  p1 <- data.frame(p)
+  p1$Status <- c("Auxotrophy", "Prototrophy")
+  p1$nrow <- nrow(Auxotrophy_12[Auxotrophy_12$phylum == pi])
+  p1$phylum <- pi
+  prop_all[[k]] <- p1
+  k <- k+1
+}
 
-a1 <- nrow(Auxotrophy_12[Auxotrophy_12$Status == 0 & Auxotrophy_12$phylum == "Actinobacteriota"]) / nrow(Auxotrophy_12[Auxotrophy_12$phylum == "Actinobacteriota"])
-a2 <- nrow(Auxotrophy_12[Auxotrophy_12$Status == 1 & Auxotrophy_12$phylum == "Actinobacteriota"])/ nrow(Auxotrophy_12[Auxotrophy_12$phylum == "Actinobacteriota"])
-a3 <- nrow(Auxotrophy_12[Auxotrophy_12$Status == 0 & Auxotrophy_12$phylum == "Bacteroidota"])/ nrow(Auxotrophy_12[Auxotrophy_12$phylum == "Bacteroidota"])
-a4 <- nrow(Auxotrophy_12[Auxotrophy_12$Status == 1 & Auxotrophy_12$phylum == "Bacteroidota"])/ nrow(Auxotrophy_12[Auxotrophy_12$phylum == "Bacteroidota"])
-a5 <- nrow(Auxotrophy_12[Auxotrophy_12$Status == 0 & Auxotrophy_12$phylum == "Other"]) / nrow(Auxotrophy_12[Auxotrophy_12$phylum == "Other"]) 
-a6 <- nrow(Auxotrophy_12[Auxotrophy_12$Status == 1 & Auxotrophy_12$phylum == "Other"]) / nrow(Auxotrophy_12[Auxotrophy_12$phylum == "Other"])
-a7 <- nrow(Auxotrophy_12[Auxotrophy_12$Status == 0 & Auxotrophy_12$phylum == "Firmicutes"]) / nrow(Auxotrophy_12[Auxotrophy_12$phylum == "Firmicutes"])
-a8 <- nrow(Auxotrophy_12[Auxotrophy_12$Status == 1 & Auxotrophy_12$phylum == "Firmicutes"]) / nrow(Auxotrophy_12[Auxotrophy_12$phylum == "Firmicutes"])
-a9 <- nrow(Auxotrophy_12[Auxotrophy_12$Status == 0 & Auxotrophy_12$phylum == "Fusobacteriota"]) / nrow(Auxotrophy_12[Auxotrophy_12$phylum == "Fusobacteriota"])
-a10 <- nrow(Auxotrophy_12[Auxotrophy_12$Status == 1 & Auxotrophy_12$phylum == "Fusobacteriota"]) / nrow(Auxotrophy_12[Auxotrophy_12$phylum == "Fusobacteriota"])
-a11 <- nrow(Auxotrophy_12[Auxotrophy_12$Status == 0 & Auxotrophy_12$phylum == "Proteobacteria"]) / nrow(Auxotrophy_12[Auxotrophy_12$phylum == "Proteobacteria"])
-a12 <- nrow(Auxotrophy_12[Auxotrophy_12$Status == 1 & Auxotrophy_12$phylum == "Proteobacteria"]) / nrow(Auxotrophy_12[Auxotrophy_12$phylum == "Proteobacteria"])
-
-a <- data.table(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12)
-v <- t(a)
-v1 <- data.table(v)
-v1$Phylum <- c("Actinobacteriota", "Actinobacteriota", "Bacteroidota", "Bacteroidota",
-               "Other",  "Other", "Firmicutes", "Firmicutes", "Fusobacteriota" , "Fusobacteriota", "Proteobacteria", "Proteobacteria")
-v1$Status <- c("Auxotroph","Prototroph","Auxotroph","Prototroph","Auxotroph","Prototroph","Auxotroph","Prototroph","Auxotroph","Prototroph","Auxotroph","Prototroph")
+prop_all_phyla <- rbindlist(prop_all)
+prop_all_phyla$perc <- prop_all_phyla$p/prop_all_phyla$nrow *100
 
 ####################         visualization         #############################
 
-p <- ggplot(v1, aes(Phylum, V1)) +
+p <- ggplot(prop_all_phyla, aes(phylum, perc)) +
   geom_bar(stat = "identity", aes(fill = Status)) +
   ylab("Proportion of MAGs") +
   scale_fill_manual(values = c('#fdd0a2','#fdae6b')) +
