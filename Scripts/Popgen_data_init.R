dac0b9e7d54829bd5cc97f577d8363ef6769bf26
Svenja Busche <s.busche@nutrinf.uni-kiel.de>
2022-08-23 16:25:35 +0200

    Popgen Stability analysis

---
+++ Scripts/Popgen_data_init.R
@@ -0,0 +1,47 @@
+#### Popgen init data
+
+library(vegan)
+library(stringr)
+library(MicrobiomeGS2)
+library(dplyr)
+library(ggplot2)
+library(data.table)
+library(ggpubr)
+library(MetBrewer)
+library(tidyverse)
+library(rstatix)
+library(RaschSampler)
+library(Hmisc)
+library(PResiduals)
+sybil::SYBIL_SETTINGS("SOLVER","cplexAPI")
+
+
+# get QC for MAGs
+completeness_cuttoff <- 85
+contamination_cutoff <- 2
+
+popgen_MAGQC <- fread("/mnt/nuuk/2022/MR_popgen_MGX/atlas/completeness.tsv")
+popgen_rel_mags <- popgen_MAGQC[Completeness >= completeness_cuttoff & contamination_cutoff <= contamination_cutoff, `Bin Id`]
+###models
+
+models <- fetch_model_collection("/mnt/nuuk/2022/MR_popgen_MGX/models/",
+                                 IDs = popgen_rel_mags)
+
+# Read bundancy table for popgen (not yet normalalised)
+popgen_mags_abun <- t(read.table("/mnt/nuuk/2022/MR_popgen_MGX/atlas/median_coverage_genomes.tsv"))
+popgen_mags_abun1 <- data.frame(popgen_mags_abun)
+popgen_mags_abun1$MAGs <- row.names(popgen_mags_abun1)
+
+##filtering for genomes (QC)
+popgen_rel_mags <- data.frame(popgen_rel_mags)
+popgen_mags_abun1 <- merge(popgen_mags_abun1, popgen_rel_mags, by.x="MAGs", by.y="popgen_rel_mags")
+
+
+
+# Calculate realtive abundancy table
+popgen_relabun <- data.table(as.table(popgen_mags_abun))
+setnames(popgen_relabun, c("model","sample","prop"))
+popgen_relabun[, prop := prop/sum(prop), by = sample]
+
+
+
---
+++ Scripts/Popgen_stability.R
@@ -0,0 +1,104 @@
+####
+##prepare data table about meta information
+
+##load info about all samples
+popgen_samples <- fread("/mnt/nuuk/2022/MR_popgen_MGX/atlas/atlas_samples.csv")
+popgen_samples$new_name <- sub("-L001|-L002", "", popgen_samples$Full_Name)
+
+##load first timepoint
+popgen_F1 <- fread("/mnt/nuuk/2022/MR_popgen_MGX/meta/1.metadata1.tsv")
+popgen_F1$newID <- sub("$", "_F1", popgen_F1$new_id)
+head(popgen_F1)
+colnames(popgen_F1)
+
+popgen_F1 <- popgen_F1[,c(1,2,20)]
+##load second timepoint
+popgen_F2 <- fread("/mnt/nuuk/2022/MR_popgen_MGX/meta/1.metadata2.tsv")
+popgen_F2$newID <- sub("$", "_F2", popgen_F2$new_id)
+colnames(popgen_F2) [2] <- "new_id"
+
+popgen_F2 <- popgen_F2[,c(1,2,19)]
+
+F1_F2 <- rbind(popgen_F2, popgen_F1)
+
+##merge information about time points with sample info
+
+popgen_data <- merge(F1_F2, popgen_samples, by.x= "newID", by.y="new_name")
+View(popgen_data)
+#delete sample ID with only  available information about one timepoint
+popgen_data <- popgen_data[popgen_data$atlas_name != "S26" & popgen_data$atlas_name != "S38" & popgen_data$atlas_name!= "S66"]
+popgen_data
+
+###calculate bray curtis dissimilarity between two time points
+rownames(popgen_mags_abun1) <- popgen_mags_abun1[,1]
+popgen_mags_abun1 <- t(popgen_mags_abun1)
+popgen_mags_abun1 <- data.frame(popgen_mags_abun1)
+popgen_mags_abun1$sample <- row.names(popgen_mags_abun1)
+
+test <- merge(popgen_mags_abun1, popgen_data, by.x= "sample", by.y = "atlas_name")
+test <- data.table(test)
+View(test)
+
+##create a loop
+popgen_data_names <- unique(test$SampleID)
+F1_names <- unique(popgen_F1$SampleID)
+F1_names <- F1_names[F1_names %in% popgen_data_names]
+F2_names <- unique(popgen_F2$SampleID)
+F2_names <- F2_names[F2_names %in% popgen_data_names]
+
+k <- 1
+Bray_all <- list()
+for(F1 in F1_names) {
+  print(F1)
+  for(F2 in F2_names) {
+    tmp<- test[SampleID == F1 | SampleID == F2, ]
+
+    if(tmp$new_id[1] == tmp$new_id[2]) {
+      tmp2 <- tmp[,c(2:571)]
+      df2 <- mutate_all(tmp2, function(x) as.numeric(as.character(x)))
+      Bray_tmp <-vegdist(df2, method = "bray")
+      #Bray_tmp
+      Bray <- data.table(Sample = tmp$new_id[[1]],
+                         Bray_dist = Bray_tmp)
+      Bray_all[[k]] <- Bray
+      k <- k+1
+     }
+  }
+}
+
+Bray_samples <- rbindlist(Bray_all)
+Bray_all
+
+###get abundance weight average of auxotrophies
+
+##### number of auxotrophies and diversity
+Auxotrophy$count <- rowSums(Auxotrophy == 0)
+
+numb_auxos_popgen <- merge(Auxotrophy, popgen_relabun, by.x= "Genomes", by.y="model")
+numb_auxos_popgen <- data.table(numb_auxos_popgen)
+
+numb_auxos_popgen[is.na(count), count:= 0]
+
+new <- numb_auxos_popgen[ ,sum(count*prop), by = sample]
+popgen_div_numb_auxos <- merge(new, popgen_data, by.x="sample", by.y="atlas_name")
+popgen_div_numb_auxos <- popgen_div_numb_auxos[popgen_div_numb_auxos$SampleID %in% F1_names]
+
+popgen_bray_numb_auxos <- merge(popgen_div_numb_auxos, Bray_samples, by.x="new_id", by.y="Sample")
+
+
+##correlation analysis for stability
+cor.test(popgen_bray_numb_auxos$Bray_dist, popgen_bray_numb_auxos$V1, method = "spearman", exact = FALSE)
+
+
+##visualization
+stability <- ggplot(popgen_bray_numb_auxos, aes(V1, Bray_dist,)) +
+  geom_point() +
+  geom_smooth(method=lm) +
+  theme_bw() +
+  xlab("Abundance-weighted average of auxotrophies per MAG") +
+  ylab("Bray distance") +
+  theme(axis.text.x = element_text(colour="black")) +
+  theme(axis.text.y = element_text(colour= "black")) +
+  theme(axis.title.y = element_text(size = 10, margin = margin(r = 10))) +
+  theme(axis.title.x = element_text(size = 10, margin = margin(t = 10))) 
+stability
