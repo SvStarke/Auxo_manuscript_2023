c9987ca3ea1f801ce4c6ed04c1726d1e006ce7b2
suahn393 <s.busche@nutrinf-uni-kiel.de>
2023-03-08 13:54:06 +0100

    small changes

--- Scripts/Validation_Price.R
+++ Scripts/Validation_Price.R
@@ -17,6 +17,8 @@ Auxotrophy <- data.frame(Auxotroph)
 # Auxotrophy
 Genome <- rownames(Auxotrophy)
 Auxotrophy$Genomes <- Genome
+Auxotrophy$Chor <- NULL
+fwrite(Auxotrophy, file ="output/plots/Auxo_Price_Val.csv", sep = ",")
 # ----
 Auxotrophy <- as.data.table(Auxotrophy)
 Auxotrophy
@@ -38,6 +40,8 @@ for(i in AA) {
 Proto_Perc <- rbindlist(l)
 Proto_Perc
 
+fwrite(Proto_Perc, file ="output/plots/Perc_Price_Val.csv", sep = ",")
+
 #visualization
 
 Price_Vali <- ggplot(Proto_Perc, aes(i, Perc_Proto))+
@@ -57,4 +61,21 @@ nrow(Auxotrophy[Auxotrophy$count !=0])
 
 #validation of loop
 (nrow(Auxotrophy_2[Auxotrophy_2$Prototrophy == 1 & Auxotrophy_2$Compound == "Trp"]))
-  
+
+##combine both HRGM prediction and Price predictions
+
+combined_HRGm_price <- merge(Freq_auxos, Proto_Perc, by.x= "AA", by.y= "i")
+combined_HRGm_price$Proto_HRGM <- (100-combined_HRGm_price$percentage)
+
+Price_HRGM_Vali <- ggplot(combined_HRGm_price) +
+  geom_bar(data = combined_HRGm_price,aes(x = AA, y=Perc_Proto, fill =  "Prototrophic genomes"), stat = "identity", position = "identity", alpha = 0.6) +
+  geom_point(data = combined_HRGm_price, aes(x= AA, y = Proto_HRGM, colour = "HRGM genomes"))+
+  labs(fill = "", colour = "") +
+  scale_fill_manual(values = c("grey38", "#D55E00")) +
+  theme_bw()+
+  xlab("Amino acids")+
+  ylab("Predicted prototrophies [%]")
+Price_HRGM_Vali
+
+ggsave("output/plots/Price_HRGM_Val.pdf", plot = Price_HRGM_Vali,
+       width = 7,height =4)
